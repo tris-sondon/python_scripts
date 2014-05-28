d73d3eb6c7634ed36e0d628e7a072c6c7e4dc967
Tristana Sondon <tristana.sondon@gmail.com>
2014-05-28 18:32:26 +0200

    Modified to have consitent quotation marks

--- rename_files.py
+++ rename_files.py
@@ -5,7 +5,7 @@ import os
 for filename in os.listdir("."): 
     if filename.find(" ") > 0: 
         fsp = filename.split()
-        new_filename = (''.join(fsp[0].zfill(2) + "_" + fsp[2].zfill(2) + "_" +  
+        new_filename = ("".join(fsp[0].zfill(2) + "_" + fsp[2].zfill(2) + "_" +  
                         "_".join(fsp[4:]))).replace("_-_","-")
         os.rename(filename,new_filename) 
 

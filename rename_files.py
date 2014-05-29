77bfe8bdcc764c1a954a0e8f56313c6df5d6f32d
Tristana Sondon <tristana.sondon@gmail.com>
2014-05-29 23:38:48 +0200

    Modified docstring

--- rename_files.py
+++ rename_files.py
@@ -17,6 +17,12 @@ This script will rename files that are in the format:
 
 to the following format:
 
+    01_01_Course_Introduction_(14_11).mp4
+    02_01_Regular_Expressions_(11_25).mp4
+    02_02_Regular_Expressions_in_Practical_NLP_(6_04).mp4
+    10_01_What_is_Relation_Extraction__(9_47).mp4
+    10_02_Using_Patterns_to_Extract_Relations_(6_17).mp4
+
 """
 
 import os

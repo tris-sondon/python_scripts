57570efb4a593135ea646918a81418fd8212d789
Tristana Sondon <tristana.sondon@gmail.com>
2014-05-30 09:39:50 +0200

    Cosmetic Changes

--- rename_files.py
+++ rename_files.py
@@ -44,9 +44,9 @@ for filename in os.listdir("."):
         new_filename = ("".join(fsp[0].zfill(2) + "_" + fsp[2].zfill(2) + "_" +
                         "_".join(fsp[4:]))).replace("_-_","-")
 
-    try:
-        os.rename(filename, new_filename)
-    except:
-        print "Exception: ", str(sys.exc_info())
+        try:
+            os.rename(filename, new_filename)
+        except:
+            print "Exception: ", str(sys.exc_info())
 
 
